b9e9839e654567f90cfcad014cbf31ffd9151d06
Ian Andriani Gonçalves <ian@extradigital.com.br>
2025-09-23 17:52:32 -0300

    chore: Restruturando estrutura do projeto

--- index.js
+++ index.js
@@ -1,37 +1,23 @@
+const path = require("path");
 const express = require("express");
-const User = require('./src/model/user');
+
 const app = express();
 
+const usersRouter = require('./src/routes/users.routes');
+app.use("/users", usersRouter);
+
+// midlewares globais
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(__dirname + "/public"));
 
+
+// rota inicial
 app.get('/', (req, resp) => {
     resp.sendFile(__dirname + '/public/user_selection.html');
 });
 
-app.get('/users', (req, resp) => {
-    resp.sendFile(__dirname + '/public/new_user.html');
-});
 
-app.post('/users', async (req, resp) => {
-    let username = req.body?.username;
-    if (username == undefined) {
-        resp.send(`Field not provided username`);
-        return; s
-    }
-
-    let is_admin = false;
-    if (req.body?.privilege == 'admin') {
-        is_admin = true;
-    }
-
-    let user = new User({ username: username, is_admin: is_admin });
-    await user.save();
-
-    resp.send('Ok');
-});
-
-app.listen(4000, function () {
-    console.log("Servidor rodando..");
+app.listen(8000, function () {
+    console.log("Servidor rodando na porta 8000...");
 });
--- src/model/user.js
+++ src/model/user.js
@@ -1,4 +1,4 @@
-const mongoose = require('../connection');
+const mongoose = require('../config/connection');
 
 const user_shema = new mongoose.Schema({
     username: String,
