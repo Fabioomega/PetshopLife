b5c0acf7075a59c46e12f9b3d549440cfe38345f
Fabioomega <fabioms2002@gmail.com>
2025-09-24 15:42:55 -0300

    Added list all users route

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,18 +1,19 @@
 const path = require("path");
 const express = require("express");
 const User = require('../model/user');
+const mongoose = require('../config/connection');
 
 const router = express.Router();
 
 router.get('/', (req, resp) => {
-    resp.sendFile(path.join(__dirname, '..', '..', 'public', 'new_user.html'));
+    resp.sendFile(path.join(__dirname, '..', '..', 'public', 'html', 'new_user.html'));
 });
 
 router.post('/', async (req, resp) => {
     let username = req.body?.username;
     if (username == undefined) {
         resp.send(`Field not provided username`);
-        return; 
+        return;
     }
 
     let is_admin = false;
@@ -24,6 +25,11 @@ router.post('/', async (req, resp) => {
     await user.save();
 
     resp.send('Ok');
+});
+
+router.get('/list', async (req, resp) => {
+    let users = await User.find({}, "username -_id");
+    resp.send(users.map((user) => user.username));
 });
 
 module.exports = router;
